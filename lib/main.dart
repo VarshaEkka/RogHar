71f43507aea4a9b980d7b7454dd860cf8f3746ed
Varsha Ekka <143595883+VarshaEkka@users.noreply.github.com>
2025-04-05 18:36:13 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(backgroundColor: Color.fromARGB(0, 245, 245, 245), body: Center(child: Text("Samarpit Seva, Swasthya Samaj"))),
+  ),
+  );
+}
